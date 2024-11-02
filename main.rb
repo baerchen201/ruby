ec06b118519a477cb4a3a7d2e9124ab81ee5cc70
baer1 <videocreator@outlook.de>
2024-11-02 15:18:11 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+print("Hello, World!\n")
+puts("Hello, \\n!")
+
+for i in 0..4
+  puts("Hello," + " #{i}")
+end
+
+print("Hello, formatted code!")
+# https://marketplace.visualstudio.com/items?itemName=baer1.vscode-rufo-fork
