96c1eda2502c2a69bc1c3da8fb28c76fda5ce8fb
baer1 <videocreator@outlook.de>
2024-11-02 15:56:30 +0100

    Add buggy code

--- main.rb
+++ main.rb
@@ -5,5 +5,14 @@ for i in 0..4
   puts("Hello," + " #{i}")
 end
 
-print("Hello, formatted code!")
 # https://marketplace.visualstudio.com/items?itemName=baer1.vscode-rufo-fork
+print("Hello, formatted code!\n")
+
+# Very buggy idk why
+puts("Enter something:")
+input = ""
+while input[-1] != "\n"
+  input += STDIN.read(length = 1)
+  print(input[-1].bytes)
+end
+print(input)
